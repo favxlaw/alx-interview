c6872084662fe21ea12090a49f330cb01d2366a2
favxlaw <nmesomafavour2004@gmail.com>
2023-09-29 11:40:11 +0100

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,12 +4,14 @@ representing the pascal's triangle of n"""
 
 
 def pascal_triangle(n):
+    """returns a list of list of integers"""
     if n <= 0:
         return []
 
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
+        
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
