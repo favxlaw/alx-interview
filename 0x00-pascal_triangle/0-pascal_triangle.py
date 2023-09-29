2ba6d5df39336246eeaf4de8f033e34d4eb94791
favxlaw <nmesomafavour2004@gmail.com>
2023-09-29 13:30:47 +0100

    indentation_update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
-        
+
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
